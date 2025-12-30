d384e9692ce7d2c377b0df43ab42f1d62d030001
Faseeh Ul Hassan <bdjkswfm5n@privaterelay.appleid.com>
2025-12-31 00:52:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ GENERATOR_MODEL = "gemini-2.5-flash"
 FIREWALL_MODEL = "gemini-2.5-flash"
 
 # Use separate keys in real deployments
-GENERATOR_API_KEY = "AIzaSyBMyNrdDAWX_nVWuqhDUipgaI9fvPeb6BA"
-FIREWALL_API_KEY = "AIzaSyA8UlxDupu5pORpUjOkddh3IkCo5td5I0I"
+GENERATOR_API_KEY = "PUT YOUR API KEY HERE"
+FIREWALL_API_KEY = "PUT YOUR API KEY HERE"
